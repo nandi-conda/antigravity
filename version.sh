11b4b8c1a168dff81085705a2527f5229234e34c
AlphaLynx <alphalynx@alphalynx.dev>
2026-01-24 15:26:35 -0500

    Add silent options to curl in version script

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Find version information to fill in PKGBUILD
-echo "x86 version information: " && curl -L https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/dists/antigravity-debian/main/binary-amd64/Packages | grep -E "^(Package|Version|MD5sum):" | tail -2
-echo "aarch64 version information: " &&curl -L https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/dists/antigravity-debian/main/binary-arm64/Packages | grep -E "^(Package|Version|MD5sum):" | tail -2
+echo "x86_64 version information: " && curl -fsSL https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/dists/antigravity-debian/main/binary-amd64/Packages | grep -E "^(Package|Version|MD5sum):" | tail -2
+echo "aarch64 version information: " && curl -fsSL https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/dists/antigravity-debian/main/binary-arm64/Packages | grep -E "^(Package|Version|MD5sum):" | tail -2
